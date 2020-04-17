740f9523a34699254f8914bf8924158ac9739723
Paul Abernathy <paul.abernathy7@gmail.com>
2020-04-16 21:43:21 -0400

    seperated some code

--- api/server.js
+++ api/server.js
@@ -1,11 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const server = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const PORT = process.env.PORT || 3000;
+
+const server = express(); // define the express app
 
 server.use(bodyParser.json());
 
 server.use(
-    bodyParser.urlencoded({
-        extended: true
-    })
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
+
+server.use(cors()); //cors requests
+server.use(helmet()); //security
+server.use(morgan()); //log http requests
+
+const userDB = require("./routes/userRoutes");
+
+server.get("/", (req, res) => {
+  res.json({
+    info: "Server is working as expected",
+  });
+});
+
+//Postgres user routes
+server.get("/users", userDB.getUsers);
+server.get("/users/:id", userDB.getUserById);
+server.post("/users", userDB.createUser);
+server.put("/users/:id", userDB.updateUser);
+server.delete("/users/:id", userDB.deleteUser);
+server.post("/users/login", userDB.userLogin);
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,40 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const helmet = require("helmet");
-const morgan = require("morgan");
+const server = require("./api/server");
+
 const PORT = process.env.PORT || 3000;
 
-const server = express(); // define the express app
-
-server.use(bodyParser.json());
-
-server.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-
-server.use(cors()); //cors requests
-server.use(helmet()); //security
-server.use(morgan()); //log http requests
-
-const userDB = require("./routes/userRoutes");
-
-server.get("/", (req, res) => {
-  res.json({
-    info: "Server is working as expected",
-  });
-});
-
-//Postgres user routes
-server.get("/users", userDB.getUsers);
-server.get("/users/:id", userDB.getUserById);
-server.post("/users", userDB.createUser);
-server.put("/users/:id", userDB.updateUser);
-server.delete("/users/:id", userDB.deleteUser);
-server.post("/users/login", userDB.userLogin);
-
 server.listen(PORT, () => {
-  console.log("***Server is listening***");
+  console.log(`Listening on port ${PORT}`);
 });
