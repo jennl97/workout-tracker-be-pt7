f0daa5a83aebfc35c86cb588a9acc89563bcce24
Paul Abernathy <paul.abernathy7@gmail.com>
2020-04-16 22:34:19 -0400

    nested routes and removed reference to userRouter

--- api/server.js
+++ api/server.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const PORT = process.env.PORT || 3000;
 
 const server = express(); // define the express app
 
@@ -19,7 +18,7 @@ server.use(cors()); //cors requests
 server.use(helmet()); //security
 server.use(morgan()); //log http requests
 
-const userDB = require("./routes/userRoutes");
+// const userDB = require("./routes/userRoutes");
 
 server.get("/", (req, res) => {
   res.json({
@@ -28,11 +27,7 @@ server.get("/", (req, res) => {
 });
 
 //Postgres user routes
-server.get("/users", userDB.getUsers);
-server.get("/users/:id", userDB.getUserById);
-server.post("/users", userDB.createUser);
-server.put("/users/:id", userDB.updateUser);
-server.delete("/users/:id", userDB.deleteUser);
-server.post("/users/login", userDB.userLogin);
+// const userRouter = require("../routes/userRouter/userRouter");
+// server.use("/user", userRouter);
 
 module.exports = server;
